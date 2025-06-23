b7fe3a7e10efada3e917a146cbcd93b800135da9
William Diamond <williambernarddiamond@gmail.com>
2025-06-22 19:05:16 -0700

    Added functionality for multiple images

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,54 @@ import { useState } from 'react'
 import templatePhoto from './assets/download.jpg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 function App() {
   return (
     <div>
-      <Image />
-      <TopBar />
+      <HeaderTabs />
+      <Image top="100px" left="0px" photo={templatePhoto} price="$50" location="Milwaukee, WI"/>
+      <Image top="400px" left="0px" photo={templatePhoto} price="$50" location="Phoenix, AZ"/>
+      <Image top="100px" left="550px" photo={templatePhoto} price="$50" location="Phoenix, AZ"/>
     </div>
   )
 }
+function Image({top, left, photo, price, location}){
+  const containerStyle = {
+    position: "absolute", 
+    top: top,
+    left: left,
+    width: "400px",
+    textAlign: "center", 
+  };
 
-function Image(){
   return (
-    <div>
-      <img src={templatePhoto} className="photo_template" alt="Photo Template" />
-    </div>
-  )
-}
-function TopBar() {
-  return (
-    <div >
-      <h1 className="bar_top">My Top Bar</h1>
-    </div>
+    <figure style={containerStyle}>
+      <img src={photo} alt="Photo" style={{ width: "100%" }} />
+      <figcaption>
+        <div>{price}</div>
+        <div>{location}</div>
+      </figcaption>
+    </figure>
   );
 }
 
+function HeaderTabs() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (_event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="header-tabs" >
+      <Tabs value={value} onChange={handleChange}>
+        <Tab label="Buy" className="custom-tab1"/>
+        <Tab label="Sell" className="custom-tab2"/>
+        <Tab label="Profile" className="custom-tab3"/>
+      </Tabs>
+    </div>
+  );
+}
 export default App
